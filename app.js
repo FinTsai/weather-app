65235ad330d5074b28171ca6ff517d273d0e95ca
Fin Tsai <fin.tsai86@gmail.com>
2017-05-14 16:16:41 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/*
+  WEATHER APP:
+
+  1. Log out the current temperature for Sydney's latitude and longitude
+    (hint: complete the getWeather function, returning a promise with the result from the API call, use that promise to then log the result)
+  2. Convert the temperature from kelvin to degrees celsius
+  3. Add a form on index.html that allows a user to search for any city's weather and logs out the result.
+    (hint: google maps api from previous example)
+  4. Display the result of the users search on the DOM.
+  5. Display the type of weather too (cloudy, sunny, etc -- check the response)
+
+  --BONUS ROUND--
+  6. Add a loading indicator until you're ready to display the information for the city.
+  7. Make it pretty, ideas:
+    * Change the background of the page to reflect the temperature
+    * Add pictures to represent the type of weather -- clouds, the sun, etc.
+    * Request a new temperature every few minutes (hint: setInterval)
+    * Animate when the weather changes.
+    *
+
+
+*/
+
+let weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
+let apiKey = "72af66db614bf9fd03583352142dd7a7";
+
+function getWeather(lat, lng) {
+  let url = `${weatherUrl}?lat=${lat}&lon=${lng}&APPID=${apiKey}`
+
+}
